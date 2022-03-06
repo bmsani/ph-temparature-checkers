cb0f9cc9e829d4125f60b6055f216d3e8e1fb833
bmsani <bmsani6@gmail.com>
2022-03-07 00:21:26 +0600

    all done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const API_KEY = `7bd5fbe4ec12f52248c79c32e6bdf367`;
 const searchTemperatur = () => {
   const city = document.getElementById("city-name").value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   console.log(url);
   fetch(url)
     .then((res) => res.json())
@@ -14,5 +14,11 @@ const setInnerText = (id, text) => {
 
 const displayTemperature = (temperature) => {
     setInnerText('city-title', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+
+    const url =`http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById("weather-icon");
+    imgIcon.setAttribute('src', url)
   console.log(temperature);
 };
